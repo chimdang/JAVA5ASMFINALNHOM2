97eab224091858d16516eaf31a4db08f31a54668
NguyenDuyNguyenMinh <Beprobro123@gmail.com>
2025-10-16 18:52:51 +0700

    GioHang

--- ASMFINALJAVA5/src/main/java/poly/edu/dao/UsersDAO.java
+++ ASMFINALJAVA5/src/main/java/poly/edu/dao/UsersDAO.java
@@ -2,8 +2,9 @@ package poly.edu.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import poly.edu.entity.*;
+import java.util.*;
 
 public interface UsersDAO extends JpaRepository<Users, Integer> {
-	Users findByMail(String mail);
+	List<Users> findByMailContaining(String keyword);
 }
 
